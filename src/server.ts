c66d4f521025f7ccbfc01ac0c99b79a50c9d7bc8
Azim Chowdhury <azimchy994@gmail.com>
2023-06-08 18:17:04 +0800

    d

--- src/server.ts
+++ src/server.ts
@@ -1,45 +1,45 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config/index'
-import { green, red } from 'console-log-colors'
-import { errorLogger, logger } from './shared/logger'
-import { Server } from 'http'
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/index';
+import { green, red } from 'console-log-colors';
+import { errorLogger, logger } from './shared/logger';
+import { Server } from 'http';
 
 process.on('uncaughtException', error => {
-  errorLogger.error(error)
-  process.exit(1)
-})
+  errorLogger.error(error);
+  process.exit(1);
+});
 
-let server: Server
+let server: Server;
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info(green('database connected successfully'))
+    await mongoose.connect(config.database_url as string);
+    logger.info(green('database connected successfully'));
 
     server = app.listen(config.port, () => {
-      logger.info(green(`server running on the port ${config.port}`))
-    })
+      logger.info(green(`server running on the port ${config.port}`));
+    });
   } catch (error) {
-    errorLogger.error(red('failed to connect database '), red(error))
+    errorLogger.error(red('failed to connect database '), red(error));
   }
 
   process.on('unhandledRejection', err => {
     if (server) {
       server.close(() => {
-        errorLogger.error(err)
-        process.exit(1)
-      })
+        errorLogger.error(err);
+        process.exit(1);
+      });
     } else {
-      process.exit(1)
+      process.exit(1);
     }
-  })
+  });
 }
-bootstrap()
+bootstrap();
 
 process.on('SIGTERM', () => {
-  logger.info('sigterm is received')
+  logger.info('sigterm is received');
   if (server) {
-    server.close()
+    server.close();
   }
-})
+});
