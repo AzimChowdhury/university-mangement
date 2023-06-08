526ee32a9d8cef9dcba86371dc4e0f29740b0d87
Azim Chowdhury <azimchy994@gmail.com>
2023-06-08 19:51:18 +0800

    try catch optimized

--- src/app/modules/users/user.route.ts
+++ src/app/modules/users/user.route.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import { UserController } from './user.controller';
 import { UserValidation } from './user.validation';
 import validateRequest from '../../middlewares/validateRequest';
-import { hurrey } from 'console-log-colors';
 const router = express.Router();
 
 router.post(
