ad0d86bb9eb0132798207bca4618b937993ec77d
Azim Chowdhury <azimchy994@gmail.com>
2023-06-22 19:55:29 +0800

    authorization complete

--- src/app/modules/academicFaculty/af.route.ts
+++ src/app/modules/academicFaculty/af.route.ts
@@ -2,25 +2,54 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AFController } from './af.controller';
 import { AFValidation } from './af.validation';
+import auth from '../../middlewares/auth';
+import { ENUM_USER_ROLE } from '../../../enums/user';
 
 const router = express.Router();
 
 router.post(
   '/create-faculty',
   validateRequest(AFValidation.createFacultyZodSchema),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   AFController.createFaculty
 );
 
-router.get('/:id', AFController.getSingleFaculty);
+router.get(
+  '/:id',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AFController.getSingleFaculty
+);
 
 router.patch(
   '/:id',
   validateRequest(AFValidation.updateFacultyZodSchema),
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.FACULTY
+  ),
   AFController.updateFaculty
 );
 
-router.delete('/:id', AFController.deleteFaculty);
+router.delete(
+  '/:id',
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AFController.deleteFaculty
+);
 
-router.get('/', AFController.getAllFaculties);
+router.get(
+  '/',
+  auth(
+    ENUM_USER_ROLE.SUPER_ADMIN,
+    ENUM_USER_ROLE.ADMIN,
+    ENUM_USER_ROLE.STUDENT
+  ),
+  AFController.getAllFaculties
+);
 
 export const AFacultyRoutes = router;
